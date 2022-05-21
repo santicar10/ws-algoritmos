cc5becd25b47d028005cbef411d0cac428d83a8e
santiago cardona sanchez <sn09pro@gmail.com>
2022-05-21 12:24:14 -0500

    funcion az y eliminar mas mejjora visual

--- ejercicio 1/js/main.js
+++ ejercicio 1/js/main.js
@@ -1,15 +1,53 @@
+const factura = document.getElementById("factura");
+let productos = [
+    {
+        canti: 10,
+        precio: 3500,
+        nombreProducto: "Chaqueta Bomber",
+        catego: "Ropa"
+    },
+    {
+        canti: 12,
+        precio: 3500,
+        nombreProducto: "Muebles",
+        catego: "Hogar"
+    },
+    {
+        canti: 5,
+        precio: 3500,
+        nombreProducto: "Iphone XS",
+        catego: "Tecnologia"
+    }
+];
 
-let productos = [];
+pintarArray();
+mostrar();
+
+const inicio = document.getElementById("radioStart");
+const final = document.getElementById("radioEnd");
+
+function addProductos() {
+    if (inicio.checked == false && final.checked == false) {
+        alert("Por favor seleccione una opcion")
+    }
+    else
+    {
+        if (inicio.checked == true) {
+            agregarInicio();
+        }
+        if (final.checked == true) {
+            agregar();
+        }
+    }
+}
+
 
 //punto 1
 
 function agregar() {
     const nombreProducto = document.getElementById("nombreProducto").value;
-    console.log(nombreProducto)
-    const categoria = document.getElementById("catego").value;
-    console.log(categoria)
+    const categoria = document.getElementById("catego").value;    
     const cantidad = parseInt(document.getElementById("canti").value);
-    console.log(cantidad)
     const precio = parseInt(document.getElementById("precio").value);
     const producto = {
         "nombreProducto": nombreProducto,
@@ -17,18 +55,29 @@ function agregar() {
         "canti" : cantidad,
         "precio": precio
     }
-    productos.push(producto);
-    
-    mostrar();
+    productos.push(producto);  
+    mostrar();  
+    pintarArray();
 }
 
 function mostrar() {
-    const listaSinOrden = document.createElement("ul");
-    document.body.appendChild(listaSinOrden);
-    const ite = document.createElement("li");
+    const name1 = document.createElement("p");
+    const name2 = document.createElement("p");
+    const name3 = document.createElement("p");
+    const name4 = document.createElement("p");
+    const separate = document.createElement("hr");
     productos.forEach(item => {
-        ite.textContent = "nombre = " + item.nombreProducto + "  Categoria = " + item.catego + "  precio = "+ item.precio +"  Cantidad = " + item.canti;
-        listaSinOrden.appendChild(ite);
+
+        name1.textContent = "NOMBRE: " + item.nombreProducto;
+        name2.textContent = "CATEGORIA: " + item.catego;
+        name3.textContent = "CANTIDAD: " + item.canti;
+        name4.textContent = "PRECIO: " + item.precio;
+
+        factura.appendChild(name1);
+        factura.appendChild(name2);
+        factura.appendChild(name3);
+        factura.appendChild(name4);
+        factura.appendChild(separate);
     })
 }
 
@@ -36,11 +85,8 @@ function mostrar() {
 
 function agregarInicio() {
     const nombreProducto = document.getElementById("nombreProducto").value;
-    console.log(nombreProducto)
     const categoria = document.getElementById("catego").value;
-    console.log(categoria)
-    const cantidad = parseInt(document.getElementById("canti").value);
-    console.log(cantidad)
+    const cantidad = parseInt(document.getElementById("canti").value)
     const precio = parseInt(document.getElementById("precio").value);
     const producto = {
         "nombreProducto": nombreProducto,
@@ -49,8 +95,8 @@ function agregarInicio() {
         "precio": precio
     }
     productos.unshift(producto);
-
     mostrar();
+    pintarArray();
 }
 
 //punto 3
@@ -100,20 +146,73 @@ function BuscarPro(){
     console.log(namee)
         const objectoEncontrado = productos.find(item => item.nombreProducto === namee);
         if (objectoEncontrado != null) {
-            alert(objectoEncontrado);  
+            alert( "Producto encontrado" + JSON.stringify(objectoEncontrado));  
         } else {
             alert("No hay un producto con el nombre");
         }
 }
 
-//ordenar 
+//punto 9
+
+const eliminar = document.getElementById("eliminar");
+
+function borrar(name) {
+    console.log(name);
+    let n = 0;
+    
+    productos.forEach((producto)=>{
+        console.log("Entreeeee")
+        if (producto.nombreProducto == name) {
+            console.log(producto.nombreProducto);
+            n = producto.nombreProducto;
+        }
+    });
+    const nuevaLista = productos.filter(producto => producto.nombreProducto !== n);
+    productos = nuevaLista;
+    console.log("---------")
+    pintarArray();
+    console.log("---------")
+}
+
+eliminar.addEventListener("click", () => {
+    const name = document.getElementById("nombreProduc").value;
+    borrar(name);    
+})
+
+//punto10 
 
 function ordenarAZ(){
-    productos.forEach(item => {
-        
-        
+    productos.sort((nombre1, nombre2) => {
+        if (nombre1.nombreProducto < nombre2.nombreProducto) {
+            return -1;
+        } else {
+            if (nombre2.nombreProducto > nombre1.nombreProducto) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
     });
+    let mensaje = "";
+    productos.forEach(producto => {
+        mensaje = mensaje + "Nombre: " + producto.nombreProducto + "\n" +
+        "Categoria: " + producto.catego + "\n" +
+        "Cantidad: " + producto.canti + "\n" +
+        "Precio: " +producto.precio  + "\n" +
+        "==========================================" + "\n" ;
+    });
+    alert(mensaje);
 }
+
+function pintarArray() {
+    productos.forEach(producto => {
+        console.log("Nombre: " + producto.nombreProducto);
+        console.log("Categoria: " + producto.catego);
+        console.log("Cantidad: " + producto.canti);
+        console.log("Precio: " + producto.precio);
+    })
+}
+
 
 
 
