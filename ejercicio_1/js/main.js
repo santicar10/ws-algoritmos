b18cfc98e94637af0728b1393c0294dd7dc25862
santiago cardona sanchez <sn09pro@gmail.com>
2022-06-04 20:54:10 -0500

    cambios de ultimo momento

--- ejercicio_1/js/main.js
+++ ejercicio_1/js/main.js
@@ -7,7 +7,7 @@ mostrar();
 const inicio = document.getElementById("radioStart");
 const final = document.getElementById("radioEnd");
 const nombreProducto = document.getElementById("nombreProducto");
-const categoria = document.getElementById("catego")    
+const categoria = document.getElementById("catego")
 const cantidad = (document.getElementById("canti"));
 const precio = (document.getElementById("precio"));
 
@@ -16,29 +16,29 @@ function addProductos() {
     if (inicio.checked == false && final.checked == false) {
         alert("Por favor seleccione una opcion")
     }
-    else if (nombreProducto.value === "" ) {
+    else if (nombreProducto.value === "" || categoria.value === "" || parseInt(cantidad.value) === "" || parseInt(precio.value) === "") {
         alert("porfavor verifique los datos")
 
-    }else
-    {
+    } else {
 
         const producto = {
             "nombreProducto": nombreProducto.value,
             "catego": categoria.value,
-            "canti" : parseInt(cantidad.value),
+            "canti": parseInt(cantidad.value),
             "precio": parseInt(precio.value)
         }
 
         if (inicio.checked == true) {
-            
+
             agregarInicio(producto);
+        } else {
+            alert("por favor verifique los datos");
         }
         if (final.checked == true) {
             agregar(producto);
+        } else {
+            alert("por favor verifique los datos");
         }
-        let ele = document.querySelector('input[name="nombreProducto"]');
-        ele.value = "";
-
 
     }
 }
@@ -46,22 +46,22 @@ function addProductos() {
 //punto 1 (agregar a la lista)
 
 function agregar(producto) {
-    productos.push(producto);  
-    mostrar();  
+    productos.push(producto);
+    mostrar();
     pintarArray();
 }
 
 function mostrar() {
-    
+
     if (factura.hasChildNodes) {
         let nodes = factura.childNodes;
         while (factura.firstChild) {
             factura.removeChild(factura.firstChild);
         }
     }
-    
+
     console.log(productos.length);
-    
+
     productos.forEach(item => {
 
         const name1 = document.createElement("p");
@@ -71,7 +71,7 @@ function mostrar() {
         const separate = document.createElement("hr");
 
         console.log("holaa");
-        
+
         name1.textContent = "NOMBRE: " + item.nombreProducto;
         name2.textContent = "CATEGORIA: " + item.catego;
         name3.textContent = "CANTIDAD: " + item.canti;
@@ -85,7 +85,7 @@ function mostrar() {
     })
 }
 
-function mostrarpro(producto){
+function mostrarpro(producto) {
     alert("producto encontrado\n" + producto.nombreProducto)
 }
 
@@ -104,7 +104,7 @@ let cont1 = 0;
 let cont2 = 0;
 let cont3 = 0;
 
-function productoCatego(){
+function productoCatego() {
     productos.forEach(item => {
         if (item.catego == "Tecnologia") {
             cont1 = cont1 + 1;
@@ -116,7 +116,7 @@ function productoCatego(){
             }
         }
     });
-    alert("de la categoria tecnologia hay: "+ cont1 +" productos" + "\n" + "de la categoria hogar hay: "+ cont2 +" productos" + "\n" + "de la categoria Ropa hay: "+ cont3 +" productos" + "\n" )
+    alert("de la categoria tecnologia hay: " + cont1 + " productos" + "\n" + "de la categoria hogar hay: " + cont2 + " productos" + "\n" + "de la categoria Ropa hay: " + cont3 + " productos" + "\n")
 }
 
 //punto 4 (cantidad de todos los productos)
@@ -124,18 +124,18 @@ function productoCatego(){
 function TotalCatego() {
     let total = 0;
     total = cont1 + cont2 + cont3;
-    alert("el total de productos es de: "+total);
-    
+    alert("el total de productos es de: " + total);
+
 }
 
 //punto 5 (precio total de todos los productos)
 
-function TotalPrecio(){
+function TotalPrecio() {
     let total = 0;
     productos.forEach(item => {
-        total = total + item.precio; 
+        total = total + item.precio;
     });
-    alert("el valor total de los productos es de: "+total);
+    alert("el valor total de los productos es de: " + total);
 }
 
 //punjto 6 (agregar cantidades)
@@ -144,22 +144,22 @@ function addRes() {
     const aad = document.getElementById("radioAdd");
     const res = document.getElementById("radioRes");
 
-        if (aad.checked == true) {
-            add();
-        }
-        if (res.checked == true) {
-            Res();
-        }
+    if (aad.checked == true) {
+        add();
+    }
+    if (res.checked == true) {
+        Res();
+    }
 }
 
-function add(){
+function add() {
     const namee = document.getElementById("editPro").value;
     const cant = parseInt(document.getElementById("numCant").value);
 
     productos.forEach(item => {
-        if ( item.nombreProducto == namee) {
+        if (item.nombreProducto == namee) {
             item.canti = item.canti + cant;
-            alert("se edito con exito: "+item.nombreProducto+" la cantidad agregada ha sido de: "+cant);
+            alert("se edito con exito: " + item.nombreProducto + " la cantidad agregada ha sido de: " + cant);
         }
         mostrar()
     });
@@ -167,31 +167,31 @@ function add(){
 
 //punto 7 (eliminar cantidades)
 
-function Res(){
+function Res() {
     const namee = document.getElementById("editPro").value;
     const cant = parseInt(document.getElementById("numCant").value);
 
     productos.forEach(item => {
-        if ( item.nombreProducto == namee) {
+        if (item.nombreProducto == namee) {
             item.canti = item.canti - cant;
-            alert("se edito con exito: "+item.nombreProducto+"\n"+" la cantidad restada ha sido de: "+cant);
+            alert("se edito con exito: " + item.nombreProducto + "\n" + " la cantidad restada ha sido de: " + cant);
         }
         mostrar()
-        
+
     });
 }
 
 //punto 8 (buscar producto)
 
-function BuscarPro(){
+function BuscarPro() {
     let namee = document.getElementById("nombreProduc").value;
     console.log(namee)
-        const objectoEncontrado = productos.find(item => item.nombreProducto === namee);
-        if (objectoEncontrado != null) {
-            mostrarpro(objectoEncontrado);  
-        } else {
-            alert("No hay un producto con el nombre");
-        }
+    const objectoEncontrado = productos.find(item => item.nombreProducto === namee);
+    if (objectoEncontrado != null) {
+        mostrarpro(objectoEncontrado);
+    } else {
+        alert("No hay un producto con el nombre");
+    }
 }
 
 //punto 9 (eliminar producto)
@@ -201,8 +201,8 @@ const eliminar = document.getElementById("eliminar");
 function borrar(name) {
     console.log(name);
     let n = 0;
-    
-    productos.forEach((producto)=>{
+
+    productos.forEach((producto) => {
         if (producto.nombreProducto == name) {
             console.log(producto.nombreProducto);
             n = producto.nombreProducto;
@@ -216,12 +216,12 @@ function borrar(name) {
 
 eliminar.addEventListener("click", () => {
     const name = document.getElementById("nombreProduc").value;
-    borrar(name);    
+    borrar(name);
 })
 
 //punto 10 (ordenar en orden alfabetico)
 
-function ordenarAZ(){
+function ordenarAZ() {
     productos.sort((nombre1, nombre2) => {
         if (nombre1.nombreProducto < nombre2.nombreProducto) {
             return -1;
@@ -236,10 +236,10 @@ function ordenarAZ(){
     let mensaje = "";
     productos.forEach(producto => {
         mensaje = mensaje + "Nombre: " + producto.nombreProducto + "\n" +
-        "Categoria: " + producto.catego + "\n" +
-        "Cantidad: " + producto.canti + "\n" +
-        "Precio: " +producto.precio  + "\n" +
-        "==========================================" + "\n" ;
+            "Categoria: " + producto.catego + "\n" +
+            "Cantidad: " + producto.canti + "\n" +
+            "Precio: " + producto.precio + "\n" +
+            "==========================================" + "\n";
     });
     alert(mensaje);
 }
@@ -257,14 +257,14 @@ function pintarArray() {
 
 //local storage
 
-if(localStorage.getItem("usuario")!=null){
-    alert("Bienvenid@ "+localStorage.getItem("usuario"));
-  }
-  else{
-    let nombre= prompt("ingrese nombre");
-    localStorage.setItem("usuario",nombre);
-  }
-  
+if (localStorage.getItem("usuario") != null) {
+    alert("Bienvenid@ " + localStorage.getItem("usuario"));
+}
+else {
+    let nombre = prompt("ingrese nombre");
+    localStorage.setItem("usuario", nombre);
+}
+
 
 
 
